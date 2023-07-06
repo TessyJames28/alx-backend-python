f75092d03925557268869dc1e66f2ce39d512299
TessyJames28 <tessyjames28@gmail.com>
2023-07-06 13:59:59 +0100

    function that returns the rounded digit for a float value

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" function with basic annotation that floor floating value """
+import math
+
+
+def floor(n: float) -> int:
+    """ rounds a floating value n """
+    return math.floor(n)
