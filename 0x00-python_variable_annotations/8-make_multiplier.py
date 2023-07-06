77a944281f840c7bc359795b65ac8f71c77b6455
TessyJames28 <tessyjames28@gmail.com>
2023-07-06 20:23:52 +0100

    Function that returns a function that multiplies a float by multiplier

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Annotated function that takes a float multiplier """
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """ returns a function that multiplies a float by multiplier """
+    return lambda x: x * multiplier
