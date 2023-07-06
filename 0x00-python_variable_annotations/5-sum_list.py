adb6844e8552a1e3e8c8842553205edf5a3bb885
TessyJames28 <tessyjames28@gmail.com>
2023-07-06 14:20:55 +0100

    Sums a list of float values and returns a float

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" function that handle Types from typing module"""
+from typing import List
+
+
+def sum_list(input_list: list[float]) -> float:
+    """ function that takes list(float) as input and return float value """
+    return sum(input_list)
