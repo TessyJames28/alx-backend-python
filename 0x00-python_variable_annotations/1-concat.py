45665e0dc48ba7f84032d28390aae6f1a7fb5166
TessyJames28 <tessyjames28@gmail.com>
2023-07-06 13:51:43 +0100

    Concat and return string value

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" basic annotation for variable concatenation """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ cancat two strings and returns a string """
+    return "{}{}".format(str1, str2)
