7cf1d5901deab4abb67c38d055affd89c9c04d39
TessyJames28 <tessyjames28@gmail.com>
2023-07-06 14:44:34 +0100

    Handled requirement error

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,7 +1,9 @@
-#!/usr/bin/env python 3
+#!/usr/bin/env python3
 """ function that accepts mixed list based on complex Types """
 from typing import List, Union
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """Sums the int and float value and return a float
+    """
     return sum(mxd_lst)
