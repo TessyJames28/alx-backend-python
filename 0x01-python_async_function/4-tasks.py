3668d34a161b75c55d6f4ebb74e6dc0ab1c0744e
TessyJames28 <tessyjames28@gmail.com>
2023-07-10 14:16:39 +0100

    Task 4

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Execute a multiple coroutine at the same time with async"""
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """returns a list of all the delays"""
+    val_list = []
+    for num in range(n):
+        value = await task_wait_random(max_delay)
+        val_list.append(value)
+    return val_list
