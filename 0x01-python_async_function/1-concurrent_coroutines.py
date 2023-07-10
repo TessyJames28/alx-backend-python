6f802e2d6ac99be8e36ff015cf944c16168e093b
TessyJames28 <tessyjames28@gmail.com>
2023-07-10 13:32:23 +0100

    Executed multiple coroutine

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Execute a multiple coroutine at the same time with async"""
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """returns a list of all the delays"""
+    val_list = []
+    for num in range(n):
+        value = await wait_random(max_delay)
+        val_list.append(value)
+    return val_list
