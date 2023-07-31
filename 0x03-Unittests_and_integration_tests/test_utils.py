00053e7e8ea96eedc8072c76be67d5d29700928f
TessyJames28 <tessyjames28@gmail.com>
2023-07-31 14:15:35 +0100

    Parameterized and patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """parameterize a unit test"""
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 from parameterized import parameterized
 import unittest
 from unittest import mock
@@ -38,9 +38,32 @@ class TestGetJson(unittest.TestCase):
     def test_get_json(self, x, expected_result):
         """return a Mock object with a json method that returns test_payload"""
         with mock.patch("requests.get") as mock_response:
-            result = get_json(x)
+            get_json(x)
             mock_response.json.return_value = expected_result
             mock_response.assert_called_once_with(x)
+
+
+class TestMemoize(unittest.TestCase):
+    """class to tes memoize"""
+
+    def test_memoize(self):
+        """test test_meoize"""
+
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        my_class = TestClass()
+
+        with mock.patch.object(my_class, 'a_method') as mock_response:
+            result = my_class.a_property()
+            result = my_class.a_property()
+            mock_response.assert_called_once()
 
 
 if __name__ == "__main__":
