772dff22dfa2ab14fe530e611e3a186c5cf33185
Tessyjames <tessyjames28@gmail.com>
2025-05-31 07:57:48 +0100

    View file with simple viewset implementation

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
 
 # Create your views here.
+class ConversationViewSet(viewsets.ModelViewSet):
+    """
+    Viewset fields that handles all CRUD operations for converstions
+    ['List', 'Retrieve', 'Create', 'Update', 'Destroy']
+    """
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    
+
+class MessageViewSet(viewsets.ModelViewSet):
+    """
+    Viewset fields that handles all CRUD operations for Message
+    ['List', 'Retrieve', 'Create', 'Update', 'Destroy']
+    """
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
