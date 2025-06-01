4c362ff0d134540b130a56a8975e6649b8a1dbbb
Tessyjames <tessyjames28@gmail.com>
2025-06-01 21:14:50 +0100

    Urls

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -4,11 +4,11 @@ from rest_framework_nested.routers import NestedDefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 
 router = routers.DefaultRouter()
-# router.register(r'messages', MessageViewSet)
-router.register(r'conversations', ConversationViewSet)
+router.register(r'messages', MessageViewSet, basename='messages')
+router.register(r'conversations', ConversationViewSet, basename='conversation')
 
 nested_router = NestedDefaultRouter(router, r'conversations', lookup='conversation')
-nested_router.register(r'messages', MessageViewSet, basename='conversation-messages')
+nested_router.register(r'messages', MessageViewSet, basename='messages')
 
 urlpatterns = [
     path('', include(router.urls)),
