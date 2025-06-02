0a1c5d14bcc5b40befacd4e55d217cfd73d5016a
Tessyjames <tessyjames28@gmail.com>
2025-06-02 13:23:36 +0100

    implement test case for GithubOrgClient.org

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Test case for client.py and fixture.py files
+"""
+from unittest.mock import patch
+from client import GithubOrgClient
+from parameterized import parameterized, parameterized_class
+import unittest
+get_json = __import__("utils").get_json
+
+
+@parameterized.expand([
+    ("google"),
+    ("abc"),
+])
+
+class TestGithubOrgClient(unittest.TestCase):
+    """
+    Handles unittest for GitubOrgClient class
+    Ensures GithubOrgClient returns the correct value
+    """
+
+    def test_org(self, company):
+        """
+        Ensures get_json is called once with expected argument
+        """
+        with patch("client.GithubOrgClient") as mock_response:
+            GithubOrgClient.ORG_URL = company
+            GithubOrgClient.org()
+
+            mock_response.assert_called_once()
