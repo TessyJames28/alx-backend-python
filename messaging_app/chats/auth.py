3640b90b726a064136393368798eb39d00c3ff91
Tessyjames <tessyjames28@gmail.com>
2025-06-07 18:33:18 +0100

    Authentication for participants

--- messaging_app/chats/auth.py
+++ messaging_app/chats/auth.py
@@ -1,12 +1,12 @@
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 
-class IsAuthenticatedOrReadOnly(IsAuthenticated):
+class IsAuthenticatedOrReadOnly(BasePermission):
     """
     Custom permission class that allows authenticated users to perform any action,
     while unauthenticated users can only read (GET) data.
     """
     
     def has_permission(self, request, view):
-        if request.method in ['GET', 'HEAD', 'OPTIONS']:
+        if request.method in SAFE_METHODS: #'GET', 'HEAD', 'OPTIONS':
             return True  # Allow read-only methods for everyone
         return super().has_permission(request, view)  # Check for authenticated users for other methods
