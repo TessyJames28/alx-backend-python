21a83a858afa3ebb7d0b3378a71588fd253634be
Tessyjames <tessyjames28@gmail.com>
2025-06-08 11:24:40 +0100

    Added default pagination class

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -87,7 +87,9 @@ REST_FRAMEWORK = {
         'django_filters.rest_framework.DjangoFilterBackend',
         'rest_framework.filters.SearchFilter',
         'rest_framework.filters.OrderingFilter',
-    ],    
+    ], 
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 20   
 }
 
 AUTH_USER_MODEL = 'chats.User' 
