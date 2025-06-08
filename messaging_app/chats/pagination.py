06ccf144754818861dc0a7931b926af5b01e4e4a
Tessyjames <tessyjames28@gmail.com>
2025-06-08 11:23:21 +0100

    Defined standard pagination

---
+++ messaging_app/chats/pagination.py
@@ -0,0 +1,10 @@
+from rest_framework.pagination import PageNumberPagination
+
+
+class StandardResultSetPagination(PageNumberPagination):
+    """
+    Standard page number pagination
+    """
+    page_size = 100
+    page_size_query_param = 'page_size'
+    max_page_size = 20
