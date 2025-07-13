949b190eab3628dd60e8a53e731bd50e82ffb922
Tessyjames <tessyjames28@gmail.com>
2025-07-13 22:17:11 +0100

    Main file for batching processing

--- python-generators-0x00/2-main.py
+++ python-generators-0x00/2-main.py
@@ -4,8 +4,6 @@ processing = __import__('1-batch_processing')
 
 ##### print processed users in a batch of 50
 try:
-    print("Processing users in batches of 50:")
     processing.batch_processing(50)
 except BrokenPipeError:
-    print("Broken pipe error, terminating early.")
     sys.stderr.close()
