88c9e68a43ac2163267ceaf6958ed5395f128d51
Tessyjames <tessyjames28@gmail.com>
2025-07-21 09:06:25 +0100

    Decorator that manages database transaction

---
+++ python-decorators-0x01/2-transactional.py
@@ -0,0 +1,38 @@
+import sqlite3 
+import functools
+
+with_db_connection = __import__('1-with_db_connection').with_db_connection
+
+"""your code goes here"""
+def transactional(func):
+    """
+    Decorator that manages database transactions by
+    automatically committing or rolling back changes
+    """
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            func(*args, **kwargs)
+            conn = args[0]
+            conn.commit
+        except Exception as e:
+            conn.rollback()
+
+        finally:
+            conn.close()
+
+
+
+
+
+
+@with_db_connection 
+@transactional 
+def update_user_email(conn, user_id, new_email): 
+    cursor = conn.cursor() 
+    cursor.execute("UPDATE users SET email = ? WHERE id = ?", (new_email, user_id)) 
+
+
+#### Update user's email with automatic transaction handling 
+
+update_user_email(user_id=1, new_email='Crawford_Cartwright@hotmail.com')
